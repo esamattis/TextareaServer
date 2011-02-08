63301260d3964f80382008f86bf62d7fb45ba343
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-02-09 00:11:27 +0200

    parse commands

--- server/editserver.coffee
+++ server/editserver.coffee
@@ -3,29 +3,26 @@
 
 http = require "http"
 exec = require('child_process').exec
-io = require "socket.io"
-temp = require "temp"
 fs = require "fs"
 path = require "path"
+
+io = require "socket.io"
+cli = require( "cli").enable 'daemon'
 Inotify = require('inotify').Inotify
 
 DIR = path.join process.env['HOME'], ".externaledits"
 
+cli.parse
+    port: ['p', "Port to listen", "number", 8000 ]
+    host: ['l', "Host to listen", "string", "127.0.0.1"]
 
 
+inotify = new Inotify()
 server = http.createServer (req, res) ->
     res.writeHead(200, {'Content-Type': 'text/html'})
     res.end('<h1>Hello world</h1>')
 
-server.listen 8000
-
-
-inotify = new Inotify()
-
 socket = io.listen server
-
-now = ->
-    (new Date().getTime())
 
 
 actions =
@@ -34,21 +31,21 @@ actions =
         # also remove watches
         console.log "we should delete " + msg.uuid
 
+    delete_all: (client, msg) ->
+
     open: (client, msg) ->
         file = path.join DIR, msg.uuid
         console.log msg
-        fs.open file, "w", (err, fd) ->
-            fs.write fd, msg.textarea, msg.textarea.lenght, 0, ->
-                fs.close fd, ->
-                    inotify.addWatch 
-                        path: DIR
-                        watch_for: Inotify.IN_CLOSE_WRITE
-                        callback: (event) ->
-                            fs.readFile file, (err, data) ->
-                                msg.textarea = data.toString()
-                                client.send JSON.stringify msg
-                    if msg.spawn
-                        editor = exec msg.executable.replace(/\{file\}/, file)
+        fs.writeFile file, msg.textarea, ->
+            inotify.addWatch
+                path: DIR
+                watch_for: Inotify.IN_CLOSE_WRITE
+                callback: (event) ->
+                    fs.readFile file, (err, data) ->
+                        msg.textarea = data.toString()
+                        client.send JSON.stringify msg
+            if msg.spawn
+                editor = exec msg.executable.replace(/\{file\}/, file)
 
 
 socket.on 'connection', (client) ->
@@ -60,9 +57,11 @@ socket.on 'connection', (client) ->
             action client, msg
         else
             console.log "Bad action " + msg.action
-            
-
-
 
     client.on 'disconnect', ->
         console.log "browser disconnected"
+
+
+cli.main (args, options) ->
+    console.log "main"
+    server.listen options.port, options.host
--- server/editserver.js
+++ server/editserver.js
@@ -1,52 +1,49 @@
 (function() {
-  var DIR, Inotify, actions, exec, fs, http, inotify, io, now, path, server, socket, temp;
+  var DIR, Inotify, actions, cli, exec, fs, http, inotify, io, path, server, socket;
   http = require("http");
   exec = require('child_process').exec;
-  io = require("socket.io");
-  temp = require("temp");
   fs = require("fs");
   path = require("path");
+  io = require("socket.io");
+  cli = require("cli").enable('daemon');
   Inotify = require('inotify').Inotify;
   DIR = path.join(process.env['HOME'], ".externaledits");
+  cli.parse({
+    port: ['p', "Port to listen", "number", 8000],
+    host: ['l', "Host to listen", "string", "127.0.0.1"]
+  });
+  inotify = new Inotify();
   server = http.createServer(function(req, res) {
     res.writeHead(200, {
       'Content-Type': 'text/html'
     });
     return res.end('<h1>Hello world</h1>');
   });
-  server.listen(8000);
-  inotify = new Inotify();
   socket = io.listen(server);
-  now = function() {
-    return new Date().getTime();
-  };
   actions = {
     "delete": function(client, msg) {
       return console.log("we should delete " + msg.uuid);
     },
+    delete_all: function(client, msg) {},
     open: function(client, msg) {
       var file;
       file = path.join(DIR, msg.uuid);
       console.log(msg);
-      return fs.open(file, "w", function(err, fd) {
-        return fs.write(fd, msg.textarea, msg.textarea.lenght, 0, function() {
-          return fs.close(fd, function() {
-            var editor;
-            inotify.addWatch({
-              path: DIR,
-              watch_for: Inotify.IN_CLOSE_WRITE,
-              callback: function(event) {
-                return fs.readFile(file, function(err, data) {
-                  msg.textarea = data.toString();
-                  return client.send(JSON.stringify(msg));
-                });
-              }
+      return fs.writeFile(file, msg.textarea, function() {
+        var editor;
+        inotify.addWatch({
+          path: DIR,
+          watch_for: Inotify.IN_CLOSE_WRITE,
+          callback: function(event) {
+            return fs.readFile(file, function(err, data) {
+              msg.textarea = data.toString();
+              return client.send(JSON.stringify(msg));
             });
-            if (msg.spawn) {
-              return editor = exec(msg.executable.replace(/\{file\}/, file));
-            }
-          });
+          }
         });
+        if (msg.spawn) {
+          return editor = exec(msg.executable.replace(/\{file\}/, file));
+        }
       });
     }
   };
@@ -64,5 +61,9 @@
     return client.on('disconnect', function() {
       return console.log("browser disconnected");
     });
+  });
+  cli.main(function(args, options) {
+    console.log("main");
+    return server.listen(options.port, options.host);
   });
 }).call(this);
